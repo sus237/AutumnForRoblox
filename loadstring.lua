69c4ee65a6aafa80428e79923b2f3e7ef1e602af
sus237 <silisgaming125+1@gmail.com>
2025-10-07 16:34:08 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/AutumnV3/AutumnForRoblox/refs/heads/main/Main.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/sus237/AutumnForRoblox/refs/heads/main/Main.lua'))()
